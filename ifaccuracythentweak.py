8cfe50c108fd01d9e4f99fb4f0b1806809c0bb64
1998dreamer <64271022+1998dreamer@users.noreply.github.com>
2020-07-15 13:39:09 +0530

    Update ifaccuracythentweak.py

--- ifaccuracythentweak.py
+++ ifaccuracythentweak.py
@@ -1,8 +1,8 @@
 import os
-accuracy = os.system("cat /taskCode/accuracy.txt")
+accuracy = os.system("cat /dlcode/accuracy.txt")
 x = 'model.add(Dense(units=32, activation=\"relu\"))'
 if accuracy < 80:
-    os.system("sed -i '/sigmoid/ i {}' /taskCode/train.py".format(x))
+    os.system("sed -i '/sigmoid/ i {}' /dlcode/mycode.py".format(x))
 else:
     print("You Achieved wnated accuracy :)")
     exit()
