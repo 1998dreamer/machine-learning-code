3687327a7302c11093d28979cf982cf9f3510cc0
1998dreamer <64271022+1998dreamer@users.noreply.github.com>
2020-07-17 01:34:08 +0530

    Update sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -5,10 +5,10 @@ server.starttls()
 server.login("bhardwaj73700@gmail.com", "sonbhardwaj2!@#maamila")
 
 #sender
-from="bhardwaj73700@gmail.com"
+from = 'bhardwaj73700@gmail.com'
 
 #receiver
-to="bhardwajnbhardwaj2015@gmail.com"
+to = 'bhardwajnbhardwaj2015@gmail.com'
 
 # message 
 message_success = "finally got more than 80% accuracy"
